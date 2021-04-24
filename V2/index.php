0fbe4e7fe1665913f1a7abab1f9ce6d3dacf40ed
kidwen-byte <79046116+kidwen-byte@users.noreply.github.com>
2021-04-24 23:50:54 +0400

    Finish 100%

--- V2/index.php
+++ V2/index.php
@@ -9,28 +9,3 @@ $content = str_replace("{ H1 }", $h1, $content);
 $content = str_replace("{ YEAR }", $year, $content);
 
 echo $content;
-$a = 5;
-$b = '05';
-var_dump($a == $b);           // Почему true? Строка преобразуется в целое число.
-var_dump((int)'012345');     // Почему 12345? int преобразует строки в целые числа, 01 не целое число.
-var_dump((float)123.0 === (int)123.0); // Почему false? Происходит строгое сравнение типов данных.
-var_dump((int)0 === (int)'hello, world'); // Почему true? Если строки в начале не содержат числа, то int преобразует строки в число 0.  
-
-$a = 1;
-$b = 2;
-$a = ++$a;
-$b = --$b;
-echo $a . $b;
-
-$a = 1;
-$b = 2;
-$a = $a + $a;
-$b = $b / $b;
-echo $a . $b;
-
-$a = 1;
-$b = 2;
-$a += $a + $a;
-$b = $a - $b;
-$a -= $b;
-echo $a . $b;
